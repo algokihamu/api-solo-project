36fcc23fee8da06bd19b21cdee42fb416734111d
Yousuke Matsuhashi <algokihamu@gmail.com>
2022-11-11 11:56:37 +0900

    fix express server setup

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
+const { application } = require("express");
 const express = require("express");
 const { default: knex } = require("knex");
-const app = express();
+
 const port = 3000;
 
-app.use(express.json);
-app.use(express.urlencoded({ extended: true }));
+const setupServer = () => {
+  const app = express();
+  app.use(express.json);
+  app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
-app.get("/", (req, res) => {
-  //    return knex.select({*}).from(a);
-});
+  app.get("/", (req, res) => {
+    //    return knex.select({*}).from(a);
+  });
 
-app.listen(port, () => {
-  console.log(`Server Started on port ${port}`);
-});
+  return app;
+};
+
+module.exports = { setupServer };
